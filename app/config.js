3fe27a1f6ff94367fe0808abd40da1e2e2897d63
steve-dickinson <steve.dickinson@defra.gov.uk>
2022-11-02 13:13:28 +0000

    Update config

--- app/config.js
+++ app/config.js
@@ -1,6 +1,25 @@
-module.exports = {
+const Joi = require('joi')
+
+const schema = Joi.object({
+  port: Joi.number().default(3000),
+  env: Joi.string().default('development'),
+  rpaApi: Joi.string().required(),
+  isDev: Joi.boolean().default(false)
+})
+
+const config = {
   port: process.env.PORT || 3000,
   env: process.env.NODE_ENV || 'development',
-  rpaApi: process.env.CH_API,
+  rpaApi: process.env.RPA_API,
   isDev: process.env.NODE_ENV === 'development'
 }
+
+const result = schema.validate(config, {
+  abortEarly: false
+})
+
+if (result.error) {
+  throw new Error(`The server config is invalid. ${result.error.message}`)
+}
+
+module.exports = result.value
